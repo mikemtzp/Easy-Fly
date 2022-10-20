6ba8827187d645063d56c980216b4c270c242ae0
toMilanesi <tomi.milanesi@gmail.com>
2022-10-20 16:57:53 -0300

    Add Main component as cointainer for the main page, add route to Main component in App.js, comment porvider and store in index to avoid conflicts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import Main from './components/main/Main';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <p>Placeholder</p>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
+// import { Provider } from 'react-redux';
+// import { store } from './app/store';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import './index.css';
 
 const container = document.getElementById('root');
@@ -11,10 +11,10 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Router>
+      {/* <Provider store={store}> */}
       <App />
-    </Provider>
+      {/* </Provider> */}
+    </Router>
   </React.StrictMode>,
 );
-
-reportWebVitals();
