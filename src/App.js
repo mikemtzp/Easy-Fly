73489ec99021cb36da19e489ab672471568f8dd6
mikemtzp <mikemp8226@gmail.com>
2022-10-25 16:29:27 -0500

    Update file structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Nav from './components/nav/Nav';
-import Main from './components/main/Main';
-import SignupForm from './components/SignupForm';
-import LoginForm from './components/LoginForm';
+import Main from './pages/main/Main';
+import SignupForm from './components/user/SignupForm';
+import LoginForm from './components/user/LoginForm';
 import './App.css';
 
 function App() {
