867e59bed7c0d99edd2117d60f76c4b62089a7a2
mikemtzp <mikemp8226@gmail.com>
2022-10-27 14:15:59 -0500

    Create jet details basic component

---
+++ src/components/jet/JetDetails.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useParams, Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+function JetDetails(props) {
+  return (
+    <div>JetDetails</div>
+  )
+}
+
+JetDetails.propTypes = {};
+
+export default JetDetails;
--- src/pages/jetdetails/JetDetailsPage.js
+++ src/pages/jetdetails/JetDetailsPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import JetDetails from '../../components/jet/JetDetails';
 
 const JetDetailsPage = () => (
   <>
