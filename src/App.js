85881c56595361770cf605cb94b930e9eabc0b05
worashf <uppert83@gmail.com>
2022-10-27 16:46:25 +0300

    FIx linters

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import { useDispatch } from 'react-redux';
 import Nav from './components/nav/Nav';
 import ReservationPage from './pages/reservation/ReservationPage';
 import Main from './pages/main/Main';
 import SignupForm from './components/user/SignupForm';
 import LoginForm from './components/user/LoginForm';
 import AddJet from './pages/addjet/AddJet';
+import { getJetsThunk } from './redux/jets/jetSlice';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getJetsThunk());
+  }, [dispatch]);
+
   return (
     <Router>
       <Nav />
