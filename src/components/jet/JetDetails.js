16ce979526256e344e88efcbc1ddfcd0af194f51
mikemtzp <mikemp8226@gmail.com>
2022-10-31 12:36:31 -0600

    Add mobile style

--- src/components/jet/JetDetails.js
+++ src/components/jet/JetDetails.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import './JetDetails.scss';
 
 function JetDetails() {
   const { jets } = useSelector((state) => state.jets);
   const { id } = useParams();
+  // const params = useParams();
   const idToNum = parseInt(id, 10);
   const jet = jets.filter((jet) => jet.id === idToNum)[0];
+
+  console.log(typeof id);
+  console.log(jets);
+  console.log(jet);
 
   return (
     <section className="details-container">
@@ -16,13 +22,15 @@ function JetDetails() {
         className="details-container__image"
       />
       <div className="details-container__info">
-        <h1>{jet.name}</h1>
-        <p>{jet.category}</p>
-        <p>{jet.size}</p>
-        <p>{jet.finance_fee}</p>
-        <p>{jet.price_per_day}</p>
+        <h1 className="details-container__info-name">{jet.name}</h1>
+        <ul className="details-container__info-table">
+          <li>{jet.category}</li>
+          <li>{jet.size}</li>
+          <li>{jet.finance_fee}</li>
+          <li>{jet.price_per_day}</li>
+        </ul>
       </div>
-      <Link to="/reservation" state={jet}>
+      <Link to="/reservation" state={jet} className="details-container__reserve">
         Reserve
       </Link>
     </section>
