38e6db7c369432b3bb548d71c2e031432992ff33
worashf <uppert83@gmail.com>
2022-10-31 15:38:20 +0300

    Check authenticate on start and Add toastify container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { ToastContainer, toast } from 'react-toastify';
 import ReservationPage from './pages/reservation/ReservationPage';
 import MyReservation from './pages/myReservation/MyReservationPage';
 import Main from './pages/main/Main';
@@ -11,29 +12,62 @@ import JetDetailsPage from './pages/jetdetails/JetDetailsPage';
 import DeleteJetPage from './pages/deleteJet/DeleteJetPage';
 import { getJetsThunk } from './redux/jets/jetSlice';
 import { getReservationsThunk } from './redux/reservation/reservationSlice';
+import {
+  authenticateThunk,
+  logout,
+  setUnAuthenticated,
+} from './redux/users/userSlice';
 import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const dispatch = useDispatch();
-
+  const { token } = useSelector((state) => state.user);
   useEffect(() => {
     dispatch(getJetsThunk());
     dispatch(getReservationsThunk());
   }, [dispatch]);
 
+  useEffect(() => {
+    if (token) {
+      dispatch(authenticateThunk(token))
+        .unwrap()
+        .then(() => {})
+        .catch(() => {
+          toast.error('You need to login again!');
+          dispatch(logout());
+        });
+    } else {
+      dispatch(setUnAuthenticated());
+    }
+  }, [token]);
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/reservation" element={<ReservationPage />} />
-        <Route path="/myreservations" element={<MyReservation />} />
-        <Route path="/jets/:id" element={<JetDetailsPage />} />
-        <Route path="/add-jet" element={<AddJet />} />
-        <Route path="/delete-jet" element={<DeleteJetPage />} />
-        <Route index path="/" element={<Main />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/reservation" element={<ReservationPage />} />
+          <Route path="/myreservations" element={<MyReservation />} />
+          <Route path="/jets/:id" element={<JetDetailsPage />} />
+          <Route path="/add-jet" element={<AddJet />} />
+          <Route path="/delete-jet" element={<DeleteJetPage />} />
+          <Route index path="/" element={<Main />} />
+        </Routes>
+      </Router>
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </>
   );
 }
 
