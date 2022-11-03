b5576c11d6c63e9489979a147667ecde9bd38a7e
toMilanesi <tomi.milanesi@gmail.com>
2022-11-03 12:59:35 -0300

    Fix get token from localStorage in reservationAPI and jetAPI

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
-const api = 'https://easy-fly-backend.herokuapp.com/api/v1';
+const api = 'http://[::1]:3000/api/v1';
+// const api = 'https://easy-fly-backend.herokuapp.com/api/v1';
 
 export default api;
