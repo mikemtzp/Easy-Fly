915b7de05f84a973b6fa841ad62e50b01be17338
toMilanesi <tomi.milanesi@gmail.com>
2022-11-03 13:01:04 -0300

    Fix API url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
-const api = 'http://[::1]:3000/api/v1';
-// const api = 'https://easy-fly-backend.herokuapp.com/api/v1';
+const api = 'https://easy-fly-backend.herokuapp.com/api/v1';
 
 export default api;
