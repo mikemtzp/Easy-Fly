dfc7ee10be45af1dc815203aa1232c7235b2fd09
worashf <uppert83@gmail.com>
2022-11-04 16:53:55 +0300

    Change token access

--- src/redux/reservation/reservationAPI.js
+++ src/redux/reservation/reservationAPI.js
@@ -4,7 +4,11 @@ import api from '../../config';
 export const postReservation = async (reservation) => {
   try {
     const { token } = JSON.parse(localStorage.getItem('easy-fly-data'));
-    const res = await axios.post(`${api}/reservations`, { reservation }, { headers: { Authorization: `Bearer ${token.token}` } });
+    const res = await axios.post(
+      `${api}/reservations`,
+      { reservation },
+      { headers: { Authorization: `Bearer ${token}` } },
+    );
     return res.data;
   } catch (err) {
     throw new Error(err);
@@ -14,7 +18,9 @@ export const postReservation = async (reservation) => {
 export const getReservations = async () => {
   try {
     const { token } = JSON.parse(localStorage.getItem('easy-fly-data'));
-    const response = await axios.get(`${api}/reservations`, { headers: { Authorization: `Bearer ${token.token}` } });
+    const response = await axios.get(`${api}/reservations`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
     return response.data;
   } catch (err) {
     throw new Error('err');
@@ -24,7 +30,9 @@ export const getReservations = async () => {
 export const deleteReservation = async (reservation) => {
   try {
     const { token } = JSON.parse(localStorage.getItem('easy-fly-data'));
-    const res = await axios.delete(`${api}/reservations/${reservation.id}`, { headers: { Authorization: `Bearer ${token.token}` } });
+    const res = await axios.delete(`${api}/reservations/${reservation.id}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
     return res;
   } catch (err) {
     throw new Error(err);
